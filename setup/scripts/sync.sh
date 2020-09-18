245861a59d885bc75755cb41c1545c3d493d31c8
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-09-18 22:10:20 +0200

    misc

--- setup/scripts/sync.sh
+++ setup/scripts/sync.sh
@@ -4,10 +4,29 @@ cd "$(dirname $(readlink -f $0))"/..
 [ ! -f .env ] && exit 1
 source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
 
+ropts="-Aazv --delete --exclude=prometheus/data --exclude=/backups/ --exclude=postgresql/data.*"
 
 pwd
-systemctl stop compose-gitlab || /bin/true
-for i in data/;do
-    rsync -Aazv $OLD_HOST:${OLD_PATH}/$i/ $i/ --exclude=prometheus/data --exclude=/backups/
-done
+[[ -z "$SKIP_SYNC_STOP-1}" ]] && systemctl stop compose-gitlab || /bin/true
+sync_() {
+    local i="$1"
+    rsync $ropts  -e "ssh -o Port=$SYNC_SSH_PORT" $SYNC_HOST:${SYNC_PATH}/$i nobackup/$i
+    rsync $ropts  nobackup/$i $i
+}
+waitup() {
+    while true;do
+            content=$(curl -H "X-Forwarded-Proto: https" localhost:80/signin  2>&1 || /bin/true)
+            if ( echo $content | grep -q 'users/sign_in">redirected' );then
+                break
+            else
+                sleep 1
+            fi
+    done
+}
+[[ -z "${SKIP_SYNC_DATA-}" ]] && sync_ data/
+[[ -z "${SKIP_SYNC_LOG-}" ]] && sync_ log/
 $dc up -d --force-recreate
+{% if gitlab_change_title %}
+waitup
+$dc exec web sh -c 'echo "update appearances set title='"'$GITLAB_TITLE'"' where id=1"|gitlab-psql'
+{% endif %}
